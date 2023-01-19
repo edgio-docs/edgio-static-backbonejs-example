4364fff710ec5dd9af6547a2a8ef89fec25bc633
GitHub Action <action@github.com>
2023-01-19 00:43:34 +0000

    Publishing example examples/static-backbonejs

--- edgio.config.js
+++ edgio.config.js
@@ -1,5 +1,5 @@
 // This file was automatically added by layer0 deploy.
 // You should commit this file to source control.
 module.exports = {
-  routes: './layer0/routes.ts'
-}
+  routes: './edgio/routes.ts',
+};
--- edgio/routes.ts
+++ edgio/routes.ts
@@ -1,28 +1,28 @@
 // routes.ts
-import { Router } from '@layer0/core/router'
-import { STATIC_ASSET_CACHE } from './cache'
+import { Router } from '@edgio/core/router';
+import { STATIC_ASSET_CACHE } from './cache';
 
 export default new Router()
   .prerender([{ path: '/' }])
 
   // client-rendered appshell
   .get('/', ({ serveStatic, cache }) => {
-    cache(STATIC_ASSET_CACHE)
-    serveStatic('index.html')
+    cache(STATIC_ASSET_CACHE);
+    serveStatic('index.html');
   })
 
   // css/js resources
   .get('/js/:path*', ({ serveStatic, cache }) => {
-    cache(STATIC_ASSET_CACHE)
-    serveStatic('js/:path*')
+    cache(STATIC_ASSET_CACHE);
+    serveStatic('js/:path*');
   })
   .get('/css/:path*', ({ serveStatic, cache }) => {
-    cache(STATIC_ASSET_CACHE)
-    serveStatic('css/:path*')
+    cache(STATIC_ASSET_CACHE);
+    serveStatic('css/:path*');
   })
 
   // all other routes not matched above
   .fallback(({ serveStatic, cache }) => {
-    cache(STATIC_ASSET_CACHE)
-    serveStatic('index.html')
-  })
+    cache(STATIC_ASSET_CACHE);
+    serveStatic('index.html');
+  });
